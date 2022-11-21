78d1e266d639e01d7d915856820b4715b6bc6432
GenryEden <a926788@gmail.com>
2022-11-22 02:21:10 +0300

    implemented a lot (excepting "mark all as done" and "clear completed" buttons)

---
+++ index.js
@@ -0,0 +1,170 @@
+let todos = [];
+
+const filterModes = {
+	All: 0,
+	Active: 1,
+	Completed: 2
+}
+
+let filterMode = filterModes.All;
+let mainInputForm = document.querySelector("[class='main-input-form']");
+let mainInput = document.querySelector("[class='main-input']");
+let todoList = document.querySelector("[class='todo-list']");
+let leftCounter = document.querySelector("[class='footer-sign']");
+let filterSelectors = document.querySelectorAll("[name='tasks-selector-radio']")
+
+for (selector of filterSelectors) {
+	if (selector.checked) {
+		if (selector.value == 'all') {
+			filterMode = filterModes.All
+		} 
+		if (selector.value == 'active') {
+			filterMode = filterModes.Active
+		}
+		if (selector.value == 'completed') {
+			filterMode = filterModes.Completed
+		}
+	}
+}
+
+
+function addTodo(e) {
+	const todo = newTodo(mainInput.value);
+	mainInput.value = "";	
+	todos.push(todo);
+	console.log(todos);
+	if (filterMode != filterModes.Completed) {
+		todoList.appendChild(createNodeOfTodo(todo))
+	}
+	updateLeftCounter();
+	e.preventDefault();
+} 
+
+mainInputForm.addEventListener('submit', addTodo);
+
+function changeFilter(e) {
+	id = e.target.id;
+	if (id == 'radio-all') {
+		filterMode = filterModes.All;
+	}
+	if (id == 'radio-active') {
+		filterMode = filterModes.Active;
+	}
+	if (id == 'radio-completed') {
+		filterMode = filterModes.Completed;
+	}
+	console.log(filterMode)
+	rerenderTodos()
+}
+
+for (selector of filterSelectors) {
+	selector.addEventListener('click', changeFilter);
+}
+
+function rerenderTodos() {
+	while (todoList.firstChild) {
+		todoList.removeChild(todoList.lastChild);
+	}
+	for (todo of todos) {
+		if (isInFilter(todo)) {
+			todoList.appendChild(createNodeOfTodo(todo));
+		}
+	}
+}
+
+function isInFilter(todo) {
+	return filterMode == filterModes.All ||
+		(filterMode == filterModes.Active && todo.isActive) ||
+		(filterMode == filterModes.Completed && !todo.isActive);
+}
+
+function updateLeftCounter() {
+	cnt = 0;
+	for (todo of todos) {
+		if (todo.isActive) {
+			cnt++;
+		}
+	}
+	leftCounter.textContent = cnt + ' items left';
+}
+
+function remarkToDo(e) {
+	id = e.target.getAttribute('id').split('-')[1];
+	for (todo of todos) {
+		if (todo.rndID == id) {
+			todo.isActive = !e.target.checked;
+			break;
+		}
+	}	
+
+	if (!isInFilter(todo)) {
+		e.target.parentNode.remove();
+	}
+	updateLeftCounter();
+	console.log(todos)
+}
+
+
+function deleteToDo(e) {
+	todoNode = e.target.parentNode;
+	let id = todoNode.getAttribute('id').split('-')[2];
+	let indexToDelete = 0;
+	todos = todos.filter((value, index, arr) => value.rndID != id);
+	console.log(indexToDelete)
+	console.log(todos)
+	todoNode.remove();
+	updateLeftCounter();
+}
+
+function newTodo(name) {
+	return {
+		rndID: Date.now(),
+		label: name,
+		isActive: true
+	}
+}
+
+function createNodeOfTodo(todo) {
+	const ans = document.createElement("li");
+	ans.setAttribute('class', "todo-thing");
+	ans.setAttribute('id', "todo-thing-"+todo.rndID);
+	ans.setAttribute('aria-label', "Todo - " + todo.label);
+	ans.appendChild(createCheckboxForTodo(todo));
+	ans.appendChild(createCheckboxImageForTodo(todo));
+	ans.appendChild(createSignForTodo(todo));
+	ans.appendChild(createDeleteButtonForTodo(todo));
+	return ans;
+}
+
+function createCheckboxForTodo(todo) {
+	const ans = document.createElement("input");
+	ans.setAttribute('id', 'checkbox-'+todo.rndID);
+	ans.setAttribute('class', 'done-checkbox');
+	ans.setAttribute('type', 'checkbox');
+	ans.setAttribute('aria-label', "Mark '" + todo.label + "' as " + (todo.isActive ? "" : "un") + "done");
+	ans.checked = !todo.isActive;
+	ans.addEventListener('click', remarkToDo);
+	return ans;
+}
+
+function createCheckboxImageForTodo(todo) {
+	const ans = document.createElement("label");
+	ans.setAttribute('class', 'checkbox-image');
+	ans.setAttribute('for', 'checkbox-'+todo.rndID);
+	return ans;
+}
+
+function createSignForTodo(todo) {
+	const ans = document.createElement("span");
+	ans.setAttribute('class', 'todo-thing-string');
+	ans.textContent = todo.label;
+	return ans;
+}
+
+function createDeleteButtonForTodo(todo) {
+	const ans = document.createElement("button");
+	ans.setAttribute("class", "delete-button");
+	ans.setAttribute("aria-label", "Delete '" + todo.label + "' task")
+	ans.addEventListener('click', deleteToDo)
+	return ans;
+}
